6bc2a439e8fa5a0a0ea39e4a2315b71d33de156b
Phillipe153 <rodrigues153@gmail.com>
2022-01-07 15:46:28 -0300

    Adiciona requisito 2

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,44 +1,68 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Form extends React.Component {
   render() {
+    const {
+      cardName,
+      cardDescription,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardImage,
+      cardRare,
+      cardTrunfo,
+      // hasTrunfo,
+      isSaveButtonDisabled,
+      onInputChange,
+      onSaveButtonClick,
+    } = this.props;
+
     return (
       <form>
         <fieldset>
           <input
             data-testid="name-input"
             type="text"
-            value="nome"
+            value={ cardName }
+            onChange={ onInputChange }
+
           />
           <input
             data-testid="description-input"
             type="textarea"
-            value="descriçao"
+            value={ cardDescription }
+            onChange={ onInputChange }
           />
           <input
             data-testid="attr1-input"
             type="number"
-            value="atributo 1"
+            value={ cardAttr1 }
+            onChange={ onInputChange }
           />
           <input
             data-testid="attr2-input"
             type="number"
-            value="atributo 2"
+            value={ cardAttr2 }
+            onChange={ onInputChange }
           />
           <input
             data-testid="attr3-input"
             type="number"
-            value="atributo 3"
+            value={ cardAttr3 }
+            onChange={ onInputChange }
           />
           <input
             data-testid="image-input"
             type="text"
-            value="imagem"
+            value={ cardImage }
+            onChange={ onInputChange }
           />
           <select
             data-testid="rare-input"
             type="select"
-            value="raridade"
+            value={ cardRare }
+            onChange={ onInputChange }
           >
             <option>normal</option>
             <option>raro</option>
@@ -47,11 +71,14 @@ class Form extends React.Component {
           <input
             data-testid="trunfo-input"
             type="checkbox"
-            value="nome"
+            checked={ cardTrunfo }
+            onChange={ onInputChange }
           />
           <button
             data-testid="save-button"
             type="submit"
+            disabled={ isSaveButtonDisabled }
+            onClick={ onSaveButtonClick }
           >
             Salvar
           </button>
@@ -60,5 +87,20 @@ class Form extends React.Component {
     );
   }
 }
+
+Form.propTypes = {
+  cardName: PropTypes.string.isRequired,
+  cardDescription: PropTypes.string.isRequired,
+  cardAttr1: PropTypes.string.isRequired,
+  cardAttr2: PropTypes.string.isRequired,
+  cardAttr3: PropTypes.string.isRequired,
+  cardImage: PropTypes.string.isRequired,
+  cardRare: PropTypes.string.isRequired,
+  cardTrunfo: PropTypes.bool.isRequired,
+  // hasTrunfo: PropTypes.bool.isRequired,
+  isSaveButtonDisabled: PropTypes.bool.isRequired,
+  onInputChange: PropTypes.func.isRequired,
+  onSaveButtonClick: PropTypes.func.isRequired,
+};
 
 export default Form;
